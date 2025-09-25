89d3ca46905f41a7b0d64de2aecb6e627c79fe76
Alberto <albertoriv.7@gmail.com>
2025-09-25 13:44:13 +0200

    ejercicios

--- boletin2/src/ej6/Ej6.java
+++ boletin2/src/ej6/Ej6.java
@@ -4,6 +4,8 @@ public class Ej6 {
     static void main() {
             int numeroA = 3;
             int numeroB = 4;
-
+            int prod = 0;
+            int calc= 0;
+            
         }
 }
