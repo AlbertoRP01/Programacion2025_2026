bfe8628c41a6ce204e73d54a3fd6251d8bf5c655
alber <albertoriv.7@gmail.com>
2025-09-26 01:29:22 +0200

    EJ8

---
+++ boletin2/src/ej8/Ej8.java
@@ -0,0 +1,12 @@
+package ej8;
+
+import java.util.Scanner;
+
+public class Ej8 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        int num = sc.nextInt();
+
+    }
+}
