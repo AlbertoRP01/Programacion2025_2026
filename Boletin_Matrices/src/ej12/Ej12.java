18686f234fde0a11c7c870dc71b35bc6c9540cb5
AlbertoRP01 <145667285+AlbertoRP01@users.noreply.github.com>
2025-10-28 12:53:07 +0100

    Add files via upload

---
+++ Boletin_Matrices/src/ej12/Ej12.java
@@ -0,0 +1,18 @@
+package ej12;
+
+public class Ej12 {
+    static void main(String[] args) {
+
+    }
+    public static  int[][] matrizCuadrada(int[][] matriz, int[][] matriz2){
+        int fila = matriz.length;
+        int columna = matriz[0].length;
+        int[][] nuevaMatriz = new int[fila][columna];
+        for (int i = 0; i < matriz.length; i ++){
+            for (int j = 0; j < matriz[i].length; j ++){
+
+            }
+        }
+        return matriz;
+    }
+}
