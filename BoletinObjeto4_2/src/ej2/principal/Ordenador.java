7063026e30c364728db4779796f155526499b456
Alberto <albertoriv.7@gmail.com>
2025-11-25 12:56:05 +0100

    Nuevo proyecto y ejercicio

--- BoletinObjeto4_2/src/ej2/principal/Ordenador.java
+++ BoletinObjeto4_2/src/ej2/principal/Ordenador.java
@@ -6,7 +6,7 @@ import ej2.clases.Microprocesador;
 import ej2.clases.PlacaBase;
 import ej2.clases.TarjetaGrafica;
 
-public class Main {
+public class Ordenador {
     static void main(String[] args) {
         PlacaBase placaBase = new PlacaBase("MSI", "Intel® Z370 Chipset", "LGA1151");
         Microprocesador microprocesador = new Microprocesador("Intel", "i7-10700K", 8, 3.803F, "LGA1200");
@@ -22,20 +22,9 @@ public class Main {
             System.out.println(discoDuro.toString());
             System.out.println();
             System.out.println(tarjetaGrafica.toString());
-        }catch (PlacaBaseException e){
+        }catch (
+                PlacaBaseException e){
             System.out.println(e.getMessage());
-        }
-        // con el booleano
-        if (placaBase.compatible(microprocesador)){
-            System.out.println(placaBase.toString());
-            System.out.println();
-            System.out.println(microprocesador.toString());
-            System.out.println();
-            System.out.println(discoDuro.toString());
-            System.out.println();
-            System.out.println(tarjetaGrafica.toString());
-        }else{
-            System.out.println("El microprocesador no es compatible con la placa Base");
         }
     }
 }
---
+++ BoletinObjeto4_2/src/ej2/principal/Ordenador2.java
@@ -0,0 +1,28 @@
+package ej2.principal;
+
+import ej2.clases.DiscoDuro;
+import ej2.clases.Microprocesador;
+import ej2.clases.PlacaBase;
+import ej2.clases.TarjetaGrafica;
+
+public class Ordenador2 {
+    static void main(String[] args) {
+        PlacaBase placaBase = new PlacaBase("MSI", "Intel® Z370 Chipset", "LGA1151");
+        Microprocesador microprocesador = new Microprocesador("Intel", "i7-10700K", 8, 3.803F, "LGA1200");
+        DiscoDuro discoDuro = new DiscoDuro("Samsung", "SSD", 2000);
+        TarjetaGrafica tarjetaGrafica = new TarjetaGrafica("AMD", "6700xt", 12, 16, 40);
+
+        // con el booleano
+        if (placaBase.compatible(microprocesador)){
+            System.out.println(placaBase.toString());
+            System.out.println();
+            System.out.println(microprocesador.toString());
+            System.out.println();
+            System.out.println(discoDuro.toString());
+            System.out.println();
+            System.out.println(tarjetaGrafica.toString());
+        }else{
+            System.out.println("El microprocesador no es compatible con la placa Base");
+        }
+    }
+}
