937b85302b31fec6c287f43f73b2eb64f1e78c5a
alber <albertoriv.7@gmail.com>
2025-12-04 14:50:14 +0100

    actualizaado

--- BoletinObjetos4.3/src/juegocartasej1/principal/Juego.java
+++ BoletinObjetos4.3/src/juegocartasej1/principal/Juego.java
@@ -3,29 +3,47 @@ package juegocartasej1.principal;
 import juegocartasej1.clase.Baraja;
 import juegocartasej1.clase.Carta;
 import juegocartasej1.excepciones.BarajaException;
+import mientradasalida.MiEntradaSalida;
+
+import java.security.spec.RSAOtherPrimeInfo;
 
 public class Juego {
     static void main(String[] args) {
-       boolean salir = false;
-
-        System.out.println("----------Bienvenido al juego de cartas----------");
+        boolean salir = false;
+        System.out.println("----------bienvenido al juego de las cartas----------");
         Baraja baraja = new Baraja();
         baraja.crearBaraja();
-        while (!salir) {
-            System.out.println("Mostrando la baraja");
-            System.out.println(baraja.toString());
-            System.out.println("Barajando las cartas");
-            baraja.barajar();
-            System.out.println("Repartiendo Cartas");
+        int opcion = 0;
+        int  jugadores = MiEntradaSalida.leerEnteroPositivo("¿Cuantos van a jugar? ");
+        do {
+            opcion = MiEntradaSalida.leerEnteroPositivo("Introduce la opción");
+            switch (opcion){
+                case 1 -> {
+                    System.out.println(baraja.toString());
+                }
+                case 2 -> {
+                   baraja.barajar();
+                }
+                case 3 -> {
+                    try {
 
-            try {
-                baraja.repartirBaraja(4, 8);
-            } catch (BarajaException e) {
-                System.out.println(e.getMessage());
+                        baraja.repartirBaraja(jugadores, 2);
+                    }catch (BarajaException e){
+                        System.out.println(e.getMessage());
+                    }
+                }
+                case 4 -> {
+                    System.out.println("Hasta la proxima");
+                    salir = true;
+                }
+                default -> System.out.println("Error opcion no elegible solo 1-3");
             }
-            if (baraja.cartasRestantes() == 0) {
-            }
-        }
-        System.out.println("Ha salido de la partida");
+        }while (!salir);
+    }
+    public void menu(){
+        System.out.println("1.MostarBaraja");
+        System.out.println("2.Barajar");
+        System.out.println("3.Repartir");
+        System.out.println("4. salir");
     }
 }
