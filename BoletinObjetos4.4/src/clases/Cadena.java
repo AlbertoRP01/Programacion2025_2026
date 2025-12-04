dabe4398e1875acb56bceed4effccafac760c06f
alber <albertoriv.7@gmail.com>
2025-12-04 23:50:01 +0100

    actualizaado

---
+++ BoletinObjetos4.4/src/clases/Cadena.java
@@ -0,0 +1,32 @@
+package clases;
+
+public class Cadena {
+    private String cadena;
+    Character caracter;
+
+    public Cadena(Character caracter, String cadena) {
+        this.caracter = caracter;
+        this.cadena = cadena;
+    }
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public Character getCaracter() {
+        return caracter;
+    }
+
+    public void caracter() {
+        String cadenaMinuscula = cadena.toLowerCase();
+        char caracterMinuscula = Character.toLowerCase(caracter);
+
+        int contador = 0; // Inicializamos el contador
+
+       /* for () {
+
+        }
+
+        */
+    }
+}
