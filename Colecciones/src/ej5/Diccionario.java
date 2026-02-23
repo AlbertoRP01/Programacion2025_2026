c0d95c9db6c1aaf534880e3469172f07fcdd4704
alber <albertoriv.7@gmail.com>
2026-02-23 20:51:32 +0100

    nuevos ejercicios

---
+++ Colecciones/src/ej5/Diccionario.java
@@ -0,0 +1,13 @@
+package ej5;
+
+import javax.net.ssl.SSLContext;
+import java.util.*;
+
+public class Diccionario {
+    private Map<String, List<String>> mapaDiccionario;
+
+    public Diccionario() {
+        mapaDiccionario = new TreeMap<>();
+    }
+
+}
