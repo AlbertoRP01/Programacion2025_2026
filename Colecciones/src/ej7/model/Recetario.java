5953b0c65ec5ac799994e83ece69154d8692543b
alber <albertoriv.7@gmail.com>
2026-03-02 20:22:10 +0100

    Recetario modificado

--- Colecciones/src/ej7/model/Recetario.java
+++ Colecciones/src/ej7/model/Recetario.java
@@ -4,8 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Recetario {
+
+    /**
+     * la clave es el nombre de la receta y el valor es la receta
+     */
     private Map<String, Receta> recetas;
 
+    /**
+     * El constructor de recetario inicializando el mapa de recetas
+     */
     public Recetario() {
         this.recetas = new HashMap<>();
     }
