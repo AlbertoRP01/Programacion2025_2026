3564f5765c92981ccd7383d48c2886397d424588
Alberto <albertoriv.7@gmail.com>
2026-03-04 09:36:31 +0100

    Actualizado Recetario, receta, Ingrediente Main

--- Colecciones/src/ej7/model/Recetario.java
+++ Colecciones/src/ej7/model/Recetario.java
@@ -11,6 +11,7 @@ public class Recetario {
      * la clave es el nombre de la receta y el valor es la receta
      */
     private Map<String, Receta> recetas;
+
     /**
      * El constructor de recetario inicializando el mapa de recetas
      */
@@ -22,9 +23,10 @@ public class Recetario {
         return recetas;
     }
 
-    public Receta getReceta(String nombre){
+    public Receta getReceta(String nombre) {
         return recetas.get(nombre);
     }
+
     /**
      * Añade una receta al recetario.
      * Verifica si ya existe una receta con el mismo nombre y lanza una excepción en ese caso.
@@ -36,12 +38,13 @@ public class Recetario {
         if (recetas.containsKey(nuevaReceta.getNombre())) {
             throw new RecetaException("La receta ya existe");
         }
-            recetas.put(nuevaReceta.getNombre(), nuevaReceta);
+        recetas.put(nuevaReceta.getNombre(), nuevaReceta);
     }
 
     /**
      * TODO: ordena alfabeticamente el recetario,  utilice treeMap temporal para ordenar el mapa desordenado
      * de esta forma no haces un for, se ordena por su nombre y su tiempo de preparación
+     *
      * @return devuelve un toString del StringBuilder que es la lista de recetas
      * @throws RecetaException Lanza la excepcion en el caso de que el recetario está vacío
      */
@@ -53,9 +56,11 @@ public class Recetario {
         TreeMap<String, Receta> mapaOrdednado = recetas.entrySet()
                 .stream()
                 .collect(Collectors.toMap(
-                        Map.Entry :: getKey,
-                        Map.Entry :: getValue,
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
                         (oldValue, newValue) -> oldValue,
+                        // cuando compara las  recetas para ordenarlas son insensible a las minusculas y mayusculas
+                        //de esta manera no hay problemas al comparar
                         () -> new TreeMap<>(String.CASE_INSENSITIVE_ORDER)
                 ));
         mapaOrdednado.forEach((nombre, r) -> {
@@ -72,6 +77,7 @@ public class Recetario {
      * se filtra por el metodo encontrar el ingrediente para que luego devuelve la lista y luego esa lista se ordena
      * Luego se utiliza El StringBuilder para añadir el nombre y el tiempo de preparacion del recetario,
      * para luego mostrarlo en el listado
+     *
      * @param ingrediente la variable que se aloja en el parámetro
      * @return devuelve la lista del StringBuilder con el nombre de la reeceta y el tiempo de preeparación
      * @throws RecetaException lanza una excepcio si el recetario está vacío
@@ -82,7 +88,7 @@ public class Recetario {
                 .filter(entry -> entry.getValue().necesitaIngrediente(ingrediente))
                 .toList();
 
-        if (listaFiltrada.isEmpty()){
+        if (listaFiltrada.isEmpty()) {
             throw new RecetaException("No hay recetas con ingrediente" + ingrediente.getNombre());
 
         }
@@ -90,7 +96,7 @@ public class Recetario {
         listaFiltrada.sort(Comparator.comparing(entry -> entry.getValue().getTiempoPreparacion()));
 
         StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, Receta> entry : listaFiltrada){
+        for (Map.Entry<String, Receta> entry : listaFiltrada) {
             Receta r = entry.getValue();
             sb.append(r.getNombre())
                     .append(" - ")
